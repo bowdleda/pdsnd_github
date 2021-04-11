ce2581f34f0dfc1f1b3578890552987d4a1a203a
Doug Bowdle <bowdle@battelle.org>
2021-04-11 11:46:17 -0400

    GitHub class project, Part 1: initial commit

--- README.md
+++ README.md
@@ -1,17 +1,12 @@
 >**Note**: Please **fork** the current Udacity repository so that you will have a **remote** repository in **your** Github account. Clone the remote repository to your local machine. Later, as a part of the project "Post your Work on Github", you will push your proposed changes to the remote repository in your Github account.
 
-### Date created
-Include the date you created this project and README file.
+### Created: 2021-04-11
 
-### Project Title
-Replace the Project Title
+### Programming for Data Science with R
 
-### Description
-Describe what your project is about and what it does
+### Part of the submission for the Github Project
 
 ### Files used
 Include the files used
 
-### Credits
-It's important to give proper credit. Add links to any repo that inspired you or blogposts you consulted.
-
+### Source repository: https://github.com/udacity/pdsnd_github
---
+++ bikeshare.R
@@ -0,0 +1,69 @@
+# The Git instructions require the student submit a Python file, but see
+# https://knowledge.udacity.com/questions/503632 & https://knowledge.udacity.com/questions/255036 for support
+# for submitting a .R file instead.
+# This script was my submission for question 1.
+
+setwd("C:/Users/BOWDLE/Documents/Udacity/udacity-git-course/pdsnd_github")
+
+library(tidyverse)
+
+dat.NYC = read.csv('new-york-city.csv')
+dat.CHI = read.csv('chicago.csv')
+
+dat.CHI.mutated <- 
+  dat.CHI %>%
+  mutate(City = "Chicago") %>%
+  mutate(Age = (2021-Birth.Year))
+
+dat.NYC.mutated <- 
+  dat.NYC %>%
+  mutate(City = "New York City") %>%
+  mutate(Age = 2021-Birth.Year)
+
+dat.Q1.combined <- rbind(dat.CHI.mutated, dat.NYC.mutated)
+
+##
+
+sum.dat.combined <-
+  dat.Q1.combined %>%
+  filter(Gender == 'Male' | Gender == 'Female') %>%
+  filter(Birth.Year > 1920) %>%
+  na.omit() %>%
+  group_by(Gender, City) %>%
+  summarise(mean.age = mean(Age, na.rm = TRUE))
+
+##
+
+dat.Q1.combined %>%
+  filter(Gender == 'Male' | Gender == 'Female') %>%
+  filter(Birth.Year > 1920) %>%  
+  na.omit() %>%
+  ggplot(aes(x=Age)) +
+  geom_histogram(aes(y=..density..), binwidth = 1, colour="black", fill="white", na.rm=TRUE) +
+  geom_density(alpha=.2, fill="lightpink") +
+  coord_cartesian(xlim = c(0, 100)) +
+  facet_grid(City ~ Gender) +
+  labs(x = "Approximate Age (Years)",
+       y = "Density",
+       subtitle = "The approximate age distributions of Bikeshare users born after 1920",
+       caption = "Data source: Udacity Bikeshare dataset") +
+  theme_bw()
+
+dat.Q1.combined %>%
+  filter(User.Type == "Subscriber") %>%    
+  filter(Gender == 'Male' | Gender == 'Female') %>%
+  filter(Birth.Year > 1920) %>%
+  group_by(City) %>%
+  summarize(
+    city.mean.age = sprintf("%.1f", mean(Age, na.rm = TRUE)),
+    city.median.age = sprintf("%.1f", median(Age, na.rm = TRUE))
+      )
+
+dat.Q1.AOV <-
+  dat.Q1.combined %>%
+  filter(User.Type == "Subscriber") %>%    
+  filter(Gender == 'Male' | Gender == 'Female') %>%
+  filter(Birth.Year > 1920)
+
+aov.Q1 <- aov(Age ~ City, dat.Q1.AOV)
+TukeyHSD(aov.Q1)
